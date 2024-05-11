d5fcc4c762d4bde037a2533d3709df9efa02b78f
Fedor Burhonov <fedor.burgonov@mdfin.com>
2024-05-11 09:52:58 +0300

    figure_factory_v2.2.0

--- libs/figure_factory.py
+++ libs/figure_factory.py
@@ -5,23 +5,27 @@ from typing import Union
 import numpy as np
 import cv2 as cv
 
-__version__ = "2.1.1"
+__version__ = "2.2.0"
+
+Cell = Union[int, float]
 
 Matrix = tuple[
-    tuple[Union[int, float], Union[int, float], Union[int, float]],
-    tuple[Union[int, float], Union[int, float], Union[int, float]],
+    tuple[Cell, Cell, Cell],
+    tuple[Cell, Cell, Cell],
     tuple[int, int, int],
 ]
 RGBA = tuple[int, int, int, float]
-Point = tuple[int, int]
-Points = list[tuple[*Point, int]]
-Canvas = list[tuple[int, Union[int, None], Union[int, None], Union[int, None]]]
+Point = tuple[float, float]
+Points = tuple[tuple[*Point, int]]
+Canvas = tuple[int, tuple[int, int | None] | None]
 
 
 @dataclasses.dataclass
 class Config:
     """
-    args: [[[ width:int ], height:int ], depth:int ]
+    IMPORTANT: the first parameter is height
+
+    args: [ height:int, [ width:int, [ depth:int ]]]
         - If passed as a single integer, represents the same value for both width and height, and the depth is determined by the `depth` argument or by default if argument `depth` is missing
         - If passed two integers, represents (width, height), and the depth is determined by the `depth` argument or by default if argument `depth` is missing
         - If passed three integers, represents (width, height, depth), but the depth is determined by the `depth` argument or the value passed if argument `depth` is missing
@@ -59,29 +63,37 @@ class Config:
         else:
             self.cnv_props = self.default_cnv_props
 
+    def __repr__(self):
+        return f"Config:\n\tdefault_cnv_props: {self.default_cnv_props},\n\tcolor_palette: {self.color_palette}"
+
     def __str__(self) -> str:
-        return f"Default config:\n\tdefault_cnv_props: {self.default_cnv_props},\n\tcolor_palette: {self.color_palette}"  # pylint: disable=C0301
+        return repr(self)
 
 
 @dataclasses.dataclass
 class Utils:
     """Utils"""
 
-    def hex_to_rgb(self, hex_color: str) -> RGBA:
+    def hex_to_rgb(self, hex_str: str) -> RGBA:
         """Converts a HEX color to RGB"""
 
+        # r = int(hex_str[0:2], 16)
+        # g = int(hex_str[2:4], 16)
+        # b = int(hex_str[4:6], 16)
+        # return (r, g, b)
+
         rgba = []
-        hex_color = hex_color.replace("#", "")
+        hex_str = hex_str.lstrip("#")
 
-        if len(hex_color) in (3, 4):
-            return self.hex_to_rgb(f"#{''.join([hex * 2 for hex in hex_color])}")
+        if len(hex_str) in (3, 4):
+            return self.hex_to_rgb(f"#{''.join([hex * 2 for hex in hex_str])}")
 
-        if len(hex_color) == 6:
+        if len(hex_str) == 6:
             for i in range(0, 5, 2):
-                rgba.insert(0, int(hex_color[i : i + 2], 16))
-        elif len(hex_color) == 8:
+                rgba.insert(0, int(hex_str[i : i + 2], 16))
+        elif len(hex_str) == 8:
             for i in range(0, 7, 2):
-                color = int(hex_color[i : i + 2], 16)
+                color = int(hex_str[i : i + 2], 16)
 
                 if i == 6:
                     color = 1 / 255 * color
@@ -97,7 +109,8 @@ class Utils:
 
     def rgb_to_hex(self, r, g, b):
         """Converts a RGB color to HEX"""
-        return "#%02x%02x%02x" % (r, g, b)
+
+        return f"#{r:02x}{g:02x}{b:02x}"  # "#%02x%02x%02x" % (r, g, b)
 
     def deg_to_rads(self, deg: float) -> float:
         """Converts degrees to radians"""
@@ -114,29 +127,28 @@ class Utils:
 class Figure(Utils):  # pylint: disable=R0902
     """Figure"""
 
-    axis_x: int = 0
-    axis_y: int = 0
+    x: float = 0
+    y: float = 0
     stroke_width: int = 0
     fill_color: str = None
     stroke_color: str = None
     initial_props: Points = None
 
     def __init__(
-        self,
-        cnv: Canvas,
-        points: Points,
-        offset_x: int = axis_x,
-        offset_y: int = axis_y,
+        self, cnv: Canvas, points: Points, offset_x: float = x, offset_y: float = y
     ) -> None:
         self.cnv = cnv
-        self.initial_props = self.points = np.array(points, np.int32)
+        self.initial_props = self.points = points
         self.translate(offset_x, offset_y)
 
+    def __repr__(self):
+        return f"axis X:{self.x}, axis Y: {self.y}"
+
     def __str__(self) -> str:
-        return f"axis X:{self.axis_x}, axis Y: {self.axis_y}"  # pylint: disable=C0301
+        return repr(self)
 
     @staticmethod
-    def get_translate_matrix(tx: int, ty: int) -> Matrix:
+    def get_translate_matrix(tx: float, ty: float) -> Matrix:
         """Returns translate matrix"""
 
         return [
@@ -146,7 +158,7 @@ class Figure(Utils):  # pylint: disable=R0902
         ]
 
     @staticmethod
-    def get_scale_matrix(sw: int, sh: int) -> Matrix:
+    def get_scale_matrix(sw: float, sh: float) -> Matrix:
         """Returns scale matrix"""
 
         return [
@@ -167,10 +179,7 @@ class Figure(Utils):  # pylint: disable=R0902
             (0, 0, 1),
         ]
 
-    def _apply_matrix(
-        self,
-        matrix: Matrix,
-    ) -> None:
+    def _apply_matrix(self, matrix: Matrix) -> None:
         matrix = np.array(matrix).T
 
         points = np.array([]).reshape(0, 2)
@@ -179,26 +188,26 @@ class Figure(Utils):  # pylint: disable=R0902
             point = np.dot([*i, 1], matrix)
             points = np.append(points, [point[:2]], axis=0)
 
-        self.points = np.array(points, dtype=np.int32)
+        self.points = points
 
-    def translate(self, tx: int, ty: int = None) -> "Figure":
+    def translate(self, tx: float, ty: float = None) -> "Figure":
         """Translate"""
 
         ty = tx if ty is None else ty
-        self.axis_x += tx
-        self.axis_y += ty
+        self.x += tx
+        self.y += ty
         self._apply_matrix(Figure.get_translate_matrix(tx, ty))
 
         return self
 
-    def scale(self, sw: int, sh: int = None) -> "Figure":
+    def scale(self, sw: float, sh: float = None) -> "Figure":
         """Scale"""
 
         sh = sw if sh is None else sh
 
-        self._apply_matrix(Figure.get_translate_matrix(-self.axis_x, -self.axis_y))
+        self._apply_matrix(Figure.get_translate_matrix(-self.x, -self.y))
         self._apply_matrix(Figure.get_scale_matrix(sw, sh))
-        self._apply_matrix(Figure.get_translate_matrix(self.axis_x, self.axis_y))
+        self._apply_matrix(Figure.get_translate_matrix(self.x, self.y))
 
         return self
 
@@ -207,9 +216,21 @@ class Figure(Utils):  # pylint: disable=R0902
 
         rad = self.deg_to_rads(deg)
 
-        self._apply_matrix(Figure.get_translate_matrix(-self.axis_x, -self.axis_y))
+        self._apply_matrix(Figure.get_translate_matrix(-self.x, -self.y))
         self._apply_matrix(Figure.get_rotate_matrix(rad))
-        self._apply_matrix(Figure.get_translate_matrix(self.axis_x, self.axis_y))
+        self._apply_matrix(Figure.get_translate_matrix(self.x, self.y))
+
+        return self
+
+    def move(self, mx: float, my: float = None) -> "Figure":
+        """Move"""
+
+        self._apply_matrix(Figure.get_translate_matrix(-self.x, -self.y))
+
+        my = mx if my is None else my
+        self.x = mx
+        self.y = my
+        self._apply_matrix(Figure.get_translate_matrix(self.x, self.y))
 
         return self
 
@@ -224,9 +245,9 @@ class Figure(Utils):  # pylint: disable=R0902
         """Draw figure"""
 
         if matrix:
-            self._apply_matrix(Figure.get_translate_matrix(-self.axis_x, -self.axis_y))
+            self._apply_matrix(Figure.get_translate_matrix(-self.x, -self.y))
             self._apply_matrix(matrix)
-            self._apply_matrix(Figure.get_translate_matrix(self.axis_x, self.axis_y))
+            self._apply_matrix(Figure.get_translate_matrix(self.x, self.y))
 
         if not isinstance(stroke_width, bool):
             self.stroke_width = int(round(stroke_width)) if stroke_width else None
@@ -237,8 +258,8 @@ class Figure(Utils):  # pylint: disable=R0902
         if isinstance(self, Line):
             cv.line(
                 cnv if cnv is not None else self.cnv,
-                self.points[0],
-                self.points[1],
+                np.array(self.points[0], dtype=np.int32),
+                np.array(self.points[1], dtype=np.int32),
                 self.stroke_color,
                 self.stroke_width,
             )
@@ -246,9 +267,11 @@ class Figure(Utils):  # pylint: disable=R0902
             if not isinstance(fill_color, bool):
                 self.fill_color = self.hex_to_rgb(fill_color) if fill_color else None
 
+            points = [np.array(self.points, dtype=np.int32)]
+
             cv.polylines(  # pylint: disable=E1101
                 cnv if cnv is not None else self.cnv,
-                [self.points],
+                points,
                 True,
                 self.stroke_color,
                 (
@@ -260,7 +283,7 @@ class Figure(Utils):  # pylint: disable=R0902
 
             if self.fill_color:
                 cv.fillPoly(  # pylint: disable=E1101
-                    cnv if cnv is not None else self.cnv, [self.points], self.fill_color
+                    cnv if cnv is not None else self.cnv, points, self.fill_color
                 )
 
         return self
@@ -270,8 +293,8 @@ class Figure(Utils):  # pylint: disable=R0902
 
         # Needs TODO: Reset the size when given new
 
-        self.axis_x = 0
-        self.axis_y = 0
+        self.x = 0
+        self.y = 0
         self.points = self.initial_props
         self.stroke_width = 0
         self.fill_color = None
@@ -301,17 +324,15 @@ class Oval(Figure):
         self.end = end
         self.quality = quality
 
-        points = np.array(
-            [
-                (
-                    np.sin(degree * np.pi / 180) * width / 2,
-                    np.cos(degree * np.pi / 180) * height / 2,
-                )
-                for degree in range(
-                    self.start, self.end, round(20 * (1 - self.quality) + self.quality)
-                )
-            ]
-        )
+        points = [
+            (
+                np.sin(degree * np.pi / 180) * width / 2,
+                np.cos(degree * np.pi / 180) * height / 2,
+            )
+            for degree in range(
+                self.start, self.end, round(20 * (1 - self.quality) + self.quality)
+            )
+        ]
         super().__init__(cnv, points, offset_x, offset_y)
 
 
@@ -320,12 +341,7 @@ class Rectangle(Figure):
     """Rectangle"""
 
     def __init__(  # pylint: disable=R0913
-        self,
-        cnv: Canvas,
-        width,
-        height,
-        offset_x: int = 0,
-        offset_y: int = 0,
+        self, cnv: Canvas, width, height, offset_x: int = 0, offset_y: int = 0
     ) -> None:
         p1 = (width / -2, height / -2)
         p2 = (p1[0] + width, p1[1])
@@ -371,7 +387,7 @@ class Line(Figure):
     ) -> None:
         """Draw line"""
 
-        self.points = np.array([start_point, end_point], np.int32)
+        self.points = [start_point, end_point]
 
         super().draw(matrix, stroke_width, stroke_color, cnv=cnv)
 
@@ -400,12 +416,12 @@ def test():
     )
     print("-" * 10)
 
-    cnv = np.full(cfg.cnv_props, 255, dtype=np.uint8)
-
     width = cfg.cnv_props[1] / 4
     height = width
     axis_offset = 150
     rotation_angle = 45
+
+    cnv = np.full(cfg.cnv_props, 255, dtype=np.uint8)
 
     oval = Oval(cnv, width, height, axis_offset, axis_offset)
     rect = Rectangle(cnv, width, height, axis_offset, axis_offset)
@@ -463,14 +479,10 @@ def test():
         fig.scale(0.5, 1).rotate(rotation_angle * -2).translate(300, 0).draw(
             stroke_width=None
         )
+        fig.move(650, cfg.cnv_props[0] / 2).draw()
 
     line = Line(cnv, [150, 150], [200, 200], 4, cfg.color_palette[4])
-    line.draw(
-        [50, 50],
-        [100, 100],
-        stroke_width=4,
-        stroke_color="fff",
-    ).add(50, 0)
+    line.draw([50, 50], [100, 100], stroke_width=4, stroke_color="fff").add(50, 0)
 
     cv.imshow("Common Canvas", cnv)  # pylint: disable=E1101
     cv.waitKey(0)  # pylint: disable=E1101
