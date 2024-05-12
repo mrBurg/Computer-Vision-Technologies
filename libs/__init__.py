56708865df77f5080036feb60e667121bbc5ba67
Fedor Burhonov <fedor.burgonov@mdfin.com>
2024-05-12 10:06:07 +0300

    Laboratory_work_2

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,1 +1,15 @@
 """ Libs module """
+
+import sys
+import pathlib
+
+LIBS_PATH = pathlib.Path(__file__).parent.resolve()
+print(LIBS_PATH)
+
+sys.path.append(str(LIBS_PATH))
+
+# from figure_factory import *
+# import figure_factory as ff
+
+# __all__ = [*ff.__all__]
+# __all__ = ff
